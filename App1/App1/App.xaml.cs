91b03b6751485f06dfa6d827a019f6c310f3a7b6
Kinga Wrona <kingawr123@gmail.com>
2017-06-17 14:15:02 +0200

    Added menu

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new App1.MainPage();
+            MainPage = new Main();
         }
 
         protected override void OnStart()
